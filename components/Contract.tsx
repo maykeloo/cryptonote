1330a1e0a2856625fd8732ee788fde42860c786c
Michał Sukiennik <79942503+maykeloo@users.noreply.github.com>
2022-10-29 12:06:46 +0200

    front contract changes

--- components/Contract.tsx
+++ components/Contract.tsx
@@ -1,16 +1,13 @@
-import { useEffect, useState } from "react";
-import { load } from '../utils/utils'
+import { useEffect, useRef, useState } from "react";
+import { load } from '../utils/utils';
 
-
-interface Props {
-}
-
-export const Contract = ({ }: Props) => {
+export const Contract = () => {
     const [addressAccount, setAddresAccount] = useState<any>(null);
     const [networkId, setNetworkId] = useState<number>(0);
     const [refresh, setRefresh] = useState<boolean>(true);
     const [contract, setContract] = useState<any>(null);
     const [testMessage, setTestMessage] = useState<any>(null);
+    const noteInputRef = useRef<HTMLInputElement>(null)
 
     useEffect(() => {
         if (!refresh) return;
@@ -20,21 +17,27 @@ export const Contract = ({ }: Props) => {
             setNetworkId(option.networkId)
             setContract(option.cryptonoteContract)
         });
-    });
+    }, [refresh]);
+
+    const addNote = () => {
+        contract.methods.createNotes(noteInputRef.current?.value).send({from: addressAccount})
+    }
 
     useEffect(() => {
         if(contract){
-            contract.methods.notes(1).call().then((note: { text: any; }) => {
+            contract.methods.notes(1).call().then((note: { text: string; }) => {
                 setTestMessage(note.text)
             })
         }
-    });
+    }, [contract]);
     
     return (
         <div>
             <p>Metamask wallet account is: {addressAccount}</p>
             <p>Network id is: {networkId}</p>
             <p>Pierwsza wiadomść umieszczona na blockchain : {testMessage}</p>
+            <input type="text" ref={noteInputRef} />
+            <button onClick={addNote}>Add note</button>
         </div>
     );
 

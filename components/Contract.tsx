e3ae925a9416a5cf0dabe671220166dda43b9f1b
cricri312 <t1m4c1n0@gmail.com>
2022-10-30 08:07:32 +0100

    test share notes

--- components/Contract.tsx
+++ components/Contract.tsx
@@ -4,10 +4,16 @@ import { useContractState } from "./ContractContext";
 export const Contract = () => {
     const { cryptonoteContract: contract, addressAccount, networkId } = useContractState();
     const noteInputRef = useRef<HTMLInputElement>(null)
+    const noteInputIdRef = useRef<HTMLInputElement>(null)
+
 
     const addNote = () => {
         //contract.methods.deleteNote(1).send({from: addressAccount}).then(console.log)
         contract.methods.createNotes(noteInputRef.current?.value).send({from: addressAccount})
+    }
+    const shareNote = () => {
+        contract.methods._shareNotes("0xf2797FA0B874a0e9D431a20DB63161842F092d75",noteInputIdRef.current?.value).send({from: addressAccount})
+        .then(console.log)
     }
 
     useEffect(() => {
@@ -29,6 +35,9 @@ export const Contract = () => {
             <p>Pierwsza wiadomść umieszczona na blockchain : </p>
             <input type="text" ref={noteInputRef} />
             <button onClick={addNote}>Add note</button>
+            <input type="number" ref={noteInputIdRef} placeholder="id note test" />
+            <button onClick={shareNote}>Share note to another account </button>
+            
         </div>
     );
 

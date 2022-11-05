9e43136c8370df3784a3e6ff8c802f5683a2ad52
cricri312 <t1m4c1n0@gmail.com>
2022-11-05 12:03:04 +0100

    Edited IPFS_TOKEN, value from .env.local

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    IPFS_TOKEN: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweGY1NDc4ODM3QjA1OGI2MTFhODFjZGRmZUVCNDg5ODVmYjQyQ0Y5ODQiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2Njc2MDAwNTExNjcsIm5hbWUiOiJjcnlwdG9ub3RlQVBJIn0.8DesUTnP2THvUxHDL7v-5Os8MUWb2aOV438tIRUaA_U',
+    IPFS_TOKEN: process.env.IPFS_TOKEN,
   },
   swcMinify: true,
   images:{
